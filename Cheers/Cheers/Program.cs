8f4442d9a88cb801bdf920a67e5d2e047e7aa13a
MKergosien <Mary@Kergosien.net>
2015-10-13 15:42:27 -0500

    'Initial Commit'

---
+++ Cheers/Cheers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cheers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello there! What's your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi " + name);
+            name = name.ToLower();
+            foreach (char letter in name)
+            {
+                string aOrAn = "a";
+                foreach (char nonvoiced in "halfnorsemix")
+                {
+                    if (letter == nonvoiced)
+                    {
+                        aOrAn = "an";
+                    }
+                }
+                Console.WriteLine("Give me " + aOrAn + " " + letter);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        string capName = name;
+            System.Text.StringBuilder sb = new System.Text.StringBuilder(name);
+
+            for (int j = 0; j < sb.Length; j++)
+            {
+                if (Char.IsLower(sb[j]) == true)
+                    sb[j] = Char.ToUpper(sb[j]);
+            }
+            string corrected = sb.ToString();   
+            Console.WriteLine(corrected + " is GRAND!");
+/*            System.Console.WriteLine("Press any key to exit")*/;
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
